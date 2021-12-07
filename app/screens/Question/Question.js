3b80b6a84ee254d6301165e6728c8e5382c976f7
yaelahky <kiky.lenovo@gmail.com>
2021-12-07 23:56:07 +0700

    [Quiz] Create Question Page

--- app/screens/Question/Question.js
+++ app/screens/Question/Question.js
@@ -1,33 +1,180 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, FlatList, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const width = Dimensions.get('window').width;
+const BASIC_COLOR = '#5568FE';
+const BASIC_TRANSPARENT_COLOR = 'rgba(85, 105, 254, 0.1)';
+const BASIC_BLACK = '#0A1931';
+
+var MATEMATIKA = [
+  {
+    id: 1,
+    question: ' -5 + (-8) = ...',
+    options: [
+      {
+        option: 'a',
+        valueOption: '-3',
+        isRight: false,
+      },
+      {
+        option: 'b',
+        valueOption: '-5',
+        isRight: false,
+      },
+      {
+        option: 'a',
+        valueOption: '-8',
+        isRight: false,
+      },
+      {
+        option: 'a',
+        valueOption: '-13',
+        isRight: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: ' -5 + (-8) = ...',
+    options: [
+      {
+        option: 'a',
+        valueOption: '-3',
+        isRight: false,
+      },
+      {
+        option: 'b',
+        valueOption: '-5',
+        isRight: false,
+      },
+      {
+        option: 'a',
+        valueOption: '-8',
+        isRight: false,
+      },
+      {
+        option: 'a',
+        valueOption: '-13',
+        isRight: true,
+      },
+    ],
+  },
+];
 
 const Question = props => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [dataQuestion, setDataQuestion] = useState(MATEMATIKA);
+
+  const onScrollEnd = e => {
+    let pageNumberTemp = Math.min(
+      Math.max(Math.floor(e.nativeEvent.contentOffset.x / width + 0.7) + 1, 0),
+      MATEMATIKA.length,
+    );
+    setPageNumber(pageNumberTemp);
+  };
   return (
-    <ScrollView style={{padding: 16, backgroundColor: 'white'}}>
-      <View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{color: 'black', fontWeight: '600', fontSize: 20}}>
-              Matematika
-            </Text>
-            <Text style={{color: 'black', fontWeight: '600', fontSize: 20}}>
-              Matematika
-            </Text>
-          </View>
-
-          <Text style={{color: 'black', fontWeight: '600', fontSize: 20}}>
-            1/10
-          </Text>
-        </View>
+    <View style={{flex: 1, backgroundColor: '#F3F6FA'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          height: 70,
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity>
+          <Icon
+            style={{margin: 16}}
+            name="arrow-back-outline"
+            size={24}
+            color={BASIC_BLACK}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 16,
+            color: '#0A1931',
+            fontWeight: '500',
+          }}>
+          Matematika Pemula
+        </Text>
+        <TouchableOpacity>
+          <Icon
+            style={{marginHorizontal: 16, marginVertical: 8}}
+            name="help-circle-outline"
+            size={28}
+            color={BASIC_BLACK}
+          />
+        </TouchableOpacity>
       </View>
-
-      <View style={{marginTop: 36}}>
-        <Text style={{fontSize: 16, color: 'black', fontWeight: '600'}}>
-          Aporisma artinya bersifat maksimal, sinonimnya maksimal
+      <View style={{marginBottom: 16, flexDirection: 'row'}}>
+        <View
+          style={{
+            width: `${(pageNumber / dataQuestion.length) * 100}%`,
+            backgroundColor: BASIC_COLOR,
+            height: 2,
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: BASIC_TRANSPARENT_COLOR,
+            height: 2,
+          }}
+        />
+      </View>
+      <View style={{padding: 16, paddingBottom: 4}}>
+        <Text style={{fontSize: 16, color: BASIC_COLOR, fontWeight: 'bold'}}>
+          Pertanyaan {pageNumber}/{dataQuestion.length}
         </Text>
       </View>
-    </ScrollView>
+      <FlatList
+        data={dataQuestion}
+        pagingEnabled
+        horizontal
+        bounces={false}
+        onMomentumScrollEnd={onScrollEnd}
+        renderItem={({item, index}) => {
+          return (
+            <View style={{width, padding: 16, paddingTop: 0}}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  color: '#0A1931',
+                  fontWeight: 'bold',
+                }}>
+                {item.question}
+              </Text>
+
+              <View style={{marginTop: 20}}>
+                {item.options.map((e, i) => {
+                  return (
+                    <View
+                      style={{
+                        backgroundColor:
+                          i !== 0 ? BASIC_TRANSPARENT_COLOR : BASIC_COLOR,
+                        width: '100%',
+                        padding: 16,
+                        marginBottom: 12,
+                        borderRadius: 8,
+                      }}>
+                      <Text
+                        style={{
+                          fontSize: 20,
+                          color: i !== 0 ? BASIC_BLACK : 'white',
+                          fontWeight: '600',
+                        }}>
+                        {e.option}. {e.valueOption}
+                      </Text>
+                    </View>
+                  );
+                })}
+              </View>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 };
 
